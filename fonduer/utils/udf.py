a778916f24d82fc7d0863f43c1727cc667fdd29c
Johannes Rausch <jrausch@inf.ethz.ch>
2018-08-28 19:48:39 -0700

    Merged parse output queue with SQL input queue

--- fonduer/utils/udf.py
+++ fonduer/utils/udf.py
@@ -30,7 +30,7 @@ class UDFRunner(object):
     def __init__(self, udf_class, **udf_init_kwargs):
         self.logger = logging.getLogger(__name__)
         self.udf_class = udf_class
-        self.sql_session_class = SessionProcess
+        self.sql_session_class = SQLWorker
         self.sql_sessions = []
         self.udf_init_kwargs = udf_init_kwargs
         self.udfs = []
@@ -88,7 +88,6 @@ class UDFRunner(object):
             # udf.session.add_all(y for y in udf.apply(x, **kwargs))
 
         # Commit session and close progress bar if applicable
-        # udf.session.commit()
         sql_session_process.session.commit()
 
     def apply_mt(self, xs, parallelism, **kwargs):
@@ -97,25 +96,24 @@ class UDFRunner(object):
             raise ValueError("Fonduer must use PostgreSQL as a database backend.")
 
         # Fill a JoinableQueue with input objects
-        in_queue = JoinableQueue()
-        for x in xs:
-            in_queue.put(x)
+        in_queue_parser = JoinableQueue()
 
         # Use an output queue to track multiprocess progress
-        out_queue = JoinableQueue()
+        out_queue_parser = JoinableQueue()
 
-        in_queue_sentences = JoinableQueue()
-        out_queue_sentences = JoinableQueue()
+        # in_queue_sql_worker = JoinableQueue()
+        out_queue_sql_worker = JoinableQueue()
 
         # Track progress counts
-        total_count = in_queue.qsize()
+        # total_count = in_queue_parser.qsize()
+        total_count = len(xs)
         count = 0
 
         # Start UDF and SQL session processes
         for i in range(parallelism):
             udf = self.udf_class(
-                in_queue=in_queue,
-                out_queue=out_queue,
+                in_queue_parser=in_queue_parser,
+                out_queue_parser=out_queue_parser,
                 worker_id=i,
                 **self.udf_init_kwargs
             )
@@ -123,8 +121,8 @@ class UDFRunner(object):
             self.udfs.append(udf)
 
             sql_session = self.sql_session_class(
-                in_queue_sentences=in_queue_sentences,
-                out_queue_sentences=out_queue_sentences,
+                in_queue_sql_worker=out_queue_parser,
+                out_queue_sql_worker=out_queue_sql_worker,
                 worker_id=i,
             )
             self.sql_sessions.append(sql_session)
@@ -137,13 +135,23 @@ class UDFRunner(object):
         for sql_session in self.sql_sessions:
             sql_session.start()
 
-        while any([udf.is_alive() for udf in self.udfs]) and count < total_count:
-            doc_sentences = out_queue.get()
-            in_queue_sentences.put(doc_sentences)
+        fed_docs_count = 0
+        input_docs = list(xs)
 
-            y = out_queue_sentences.get()
+        while any([udf.is_alive() for udf in self.udfs]) and count < total_count:
+            # Feed documents into parser queue iteratively
+            if fed_docs_count < total_count:
+                in_queue_parser.put(input_docs[fed_docs_count])
+                fed_docs_count += 1
+            else:
+                in_queue_parser.put(UDF.QUEUE_CLOSED)
+
+            try:
+                y = out_queue_sql_worker.get()
+            except Empty:
+                continue
             # Update progress bar whenever an item is processed
-            if y == SessionProcess.TASK_DONE:
+            if y == SQLWorker.TASK_DONE:
                 count += 1
                 if self.pb is not None:
                     self.pb.update(1)
@@ -151,7 +159,7 @@ class UDFRunner(object):
                 raise ValueError("Got non-sentinal output.")
 
         # Tell sql sessions to stop waiting for new queue items
-        in_queue_sentences.put(SessionProcess.QUEUE_CLOSED)
+        out_queue_parser.put(SQLWorker.QUEUE_CLOSED)
 
         for udf in self.udfs:
             udf.join()
@@ -160,9 +168,9 @@ class UDFRunner(object):
             any([sql_session.is_alive() for sql_session in self.sql_sessions])
             and count < total_count
         ):
-            y = out_queue_sentences.get()
+            y = out_queue_sql_worker.get()
             # Update progress bar whenever an item is processed
-            if y == SessionProcess.TASK_DONE:
+            if y == SQLWorker.TASK_DONE:
                 count += 1
                 if self.pb is not None:
                     self.pb.update(1)
@@ -183,22 +191,24 @@ class UDFRunner(object):
         self.sql_sessions = []
 
 
-class SessionProcess(Process):
+class SQLWorker(Process):
     TASK_DONE = "done"
     QUEUE_CLOSED = "queueclosed"
 
-    def __init__(self, in_queue_sentences=None, out_queue_sentences=None, worker_id=0):
+    def __init__(
+        self, in_queue_sql_worker=None, out_queue_sql_worker=None, worker_id=0
+    ):
         """
-        in_queue_sentences: A Queue of input objects to process;
+        in_queue_sql_worker: A Queue of input objects to process;
         primarily for running in parallel
         """
         Process.__init__(self)
         self.daemon = True
-        self.in_queue_sentences = in_queue_sentences
-        self.out_queue_sentences = out_queue_sentences
+        self.in_queue_sql_worker = in_queue_sql_worker
+        self.out_queue_sql_worker = out_queue_sql_worker
         self.worker_id = worker_id
 
-        # Each UDF starts its own Engine
+        # Each SQL worker starts its own Engine
         # See SQLalchemy, using connection pools with multiprocessing.
         Session = new_sessionmaker()
         self.session = Session()
@@ -211,14 +221,14 @@ class SessionProcess(Process):
         """
         while True:
             try:
-                x = self.in_queue_sentences.get(True, QUEUE_TIMEOUT)
-                if x == SessionProcess.QUEUE_CLOSED:
-                    self.in_queue_sentences.put(SessionProcess.QUEUE_CLOSED)
+                x = self.in_queue_sql_worker.get(True, QUEUE_TIMEOUT)
+                if x == SQLWorker.QUEUE_CLOSED:
+                    self.in_queue_sql_worker.put(SQLWorker.QUEUE_CLOSED)
                     break
                 self.session.add_all(x)
                 self.session.commit()
-                self.in_queue_sentences.task_done()
-                self.out_queue_sentences.put(SessionProcess.TASK_DONE)
+                self.in_queue_sql_worker.task_done()
+                self.out_queue_sql_worker.put(SQLWorker.TASK_DONE)
             except Empty:
                 continue  # keep waiting for new elements to be added to queue
         self.session.close()
@@ -229,22 +239,19 @@ class SessionProcess(Process):
 
 
 class UDF(Process):
-    # TASK_DONE = "done"
+    QUEUE_CLOSED = "queueclosed"
 
-    def __init__(self, in_queue=None, out_queue=None, worker_id=0):
+    def __init__(self, in_queue_parser=None, out_queue_parser=None, worker_id=0):
         """
-        in_queue: A Queue of input objects to process; primarily for running in parallel
+        in_queue_parser: A Queue of input objects to process;
+         primarily for running in parallel
+        out_queue_parser: A Queue that is fed to SQL workers
         """
         Process.__init__(self)
         self.daemon = True
-        self.in_queue = in_queue
-        self.out_queue = out_queue
+        self.in_queue_parser = in_queue_parser
+        self.out_queue_parser = out_queue_parser
         self.worker_id = worker_id
-
-        #        # Each UDF starts its own Engine
-        #        # See SQLalchemy, using connection pools with multiprocessing.
-        #        Session = new_sessionmaker()
-        #        self.session = Session()
 
         # We use a workaround to pass in the apply kwargs
         self.apply_kwargs = {}
@@ -257,16 +264,15 @@ class UDF(Process):
         """
         while True:
             try:
-                x = self.in_queue.get(True, QUEUE_TIMEOUT)
-                # self.session.add_all(y for y in self.apply(x, **self.apply_kwargs))
+                x = self.in_queue_parser.get(True, QUEUE_TIMEOUT)
+                if x == UDF.QUEUE_CLOSED:
+                    self.in_queue_parser.put(UDF.QUEUE_CLOSED)
+                    break
                 all_sentences = [y for y in self.apply(x, **self.apply_kwargs)]
-                self.in_queue.task_done()
-                self.out_queue.put(all_sentences)
+                self.in_queue_parser.task_done()
+                self.out_queue_parser.put(all_sentences)
             except Empty:
-                break
-
-    #        self.session.commit()
-    #        self.session.close()
+                continue
 
     def apply(self, x, **kwargs):
         """This function takes in an object, and returns a generator / set / list"""
